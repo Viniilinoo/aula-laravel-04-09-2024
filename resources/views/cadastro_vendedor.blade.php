7d1e6780831714ba6fe432ae92868f3cf7bd6398
Viniilinoo <viniciuslino2105@gmail.com>
2024-09-18 22:36:07 -0300

    Projeto Avaliação não terminado

--- resources/views/cadastro_vendedor.blade.php
+++ resources/views/cadastro_vendedor.blade.php
@@ -5,21 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Criar Vendedor</title>
-    <link rel="stylesheet" href="..//">
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
     <form action="/criar_vendedor" method="post">
         @csrf
         <label for="name">Nome</label>
         <input type="text" name="name">
+        <br>
 
         <label for="matricula">Matricula</label>
         <input type="text" name="matricula">
+        <br>
 
         <label for="comissao">Comissão</label>
         <input type="text" name="comissao">
+        <br>
 
-        <input type="submit" value="Salvar">
+        <input id="btn" type="submit" value="Salvar">
     </form>
 </body>
 </html>
